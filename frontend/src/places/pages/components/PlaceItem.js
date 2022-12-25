dd02442492bc0d7c384fa065c267dbff18e44e27
mimi-uxui-dev <uxui.mimi@gmail.com>
2022-12-25 15:59:44 +0100

    made action buttons

--- frontend/src/places/pages/components/PlaceItem.js
+++ frontend/src/places/pages/components/PlaceItem.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Button from "../../../shared/components/FormElements/Button";
 import Card from "../../../shared/components/UIElements/Card";
 import "./PlaceItem.css";
 
@@ -15,9 +16,9 @@ const PlaceItem = (props) => {
           <p>{props.description}</p>
         </div>
         <div className="place_item__actions">
-          <button>View on Map</button>
-          <button>Edit</button>
-          <button>delete</button>
+          <Button inverse>View on Map</Button>
+          <Button to={`/places/${props.id}`}>Edit</Button>
+          <Button danger>delete</Button>
         </div>
       </Card>
     </li>
