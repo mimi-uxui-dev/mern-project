96122f66bc3cfacbbb79a64b93b8c9af37755af7
mimi-uxui-dev <uxui.mimi@gmail.com>
2022-12-29 07:35:34 +0100

    getting a place by user id

--- backend/routes/places-routes.js
+++ backend/routes/places-routes.js
@@ -1,28 +1,31 @@
-const express = require('express')
+const express = require("express");
 
-const router = express.Router()
+const router = express.Router();
 
 const DUMMY_PLACES = [
-    {
-      id: 'p1',
-      title: 'Empire State Building',
-      description: 'One of the most famous sky scrapers in the world!',
-      imageUrl:
-        'https://upload.wikimedia.org/wikipedia/commons/thumb/d/df/NYC_Empire_State_Building.jpg/640px-NYC_Empire_State_Building.jpg',
-      address: '20 W 34th St, New York, NY 10001',
-      location: {
-        lat: 40.7484405,
-        lng: -73.9878584
-      },
-      creator: 'u1'
-    }
+  {
+    id: "p1",
+    title: "Empire State Building",
+    description: "One of the most famous sky scrapers in the world!",
+    imageUrl:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/d/df/NYC_Empire_State_Building.jpg/640px-NYC_Empire_State_Building.jpg",
+    address: "20 W 34th St, New York, NY 10001",
+    location: { lat: 40.7484405, lng: -73.9878584 },
+    creator: "u1",
+  },
 ];
 
-router.get('/:pid', (req, res, next) => {
-    console.log('Get Request in Places')
-    const placeId = req.params.pid
-    const place = DUMMY_PLACES.find(p => p.id === placeId)
-    res.json({ place })
-})
+router.get("/:pid", (req, res, next) => {
+  console.log("Get Request in Places");
+  const placeId = req.params.pid;
+  const place = DUMMY_PLACES.find((p) => p.id === placeId);
+  res.json({ place });
+});
 
-module.exports = router
+router.get("/user/:uid", (req, res, next) => {
+  const userId = req.params.uid;
+  const place = DUMMY_PLACES.find((p) => p.creator === userId);
+  res.json({ place });
+});
+
+module.exports = router;
