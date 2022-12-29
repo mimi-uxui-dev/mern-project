8a4f8dfee2a533afca4f9d4350c88716b2da6dfa
mimi-uxui-dev <uxui.mimi@gmail.com>
2022-12-29 08:20:21 +0100

    handeling errors

--- backend/routes/places-routes.js
+++ backend/routes/places-routes.js
@@ -19,12 +19,26 @@ router.get("/:pid", (req, res, next) => {
   console.log("Get Request in Places");
   const placeId = req.params.pid;
   const place = DUMMY_PLACES.find((p) => p.id === placeId);
+
+  if (!place) {
+    const error = new Error("Could not find a place for the provided id.");
+    error.code = 404;
+    throw error;
+  }
+
   res.json({ place });
 });
 
 router.get("/user/:uid", (req, res, next) => {
   const userId = req.params.uid;
   const place = DUMMY_PLACES.find((p) => p.creator === userId);
+
+  if (!place) {
+    const error = new Error("Could not find a place for the provided id.");
+    error.code = 404;
+    next(error);
+  }
+
   res.json({ place });
 });
 
