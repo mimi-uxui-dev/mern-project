af8fda586cb237ac0fbe1c135f14a896a8da4599
mimi-uxui-dev <uxui.mimi@gmail.com>
2022-12-29 11:01:17 +0100

    adding places controller

--- backend/routes/places-routes.js
+++ backend/routes/places-routes.js
@@ -1,44 +1,12 @@
 const express = require("express");
 const HttpError = require("../models/http-error");
 
+const placesControllers = require("../controllers/places-controller");
+
 const router = express.Router();
 
-const DUMMY_PLACES = [
-  {
-    id: "p1",
-    title: "Empire State Building",
-    description: "One of the most famous sky scrapers in the world!",
-    imageUrl:
-      "https://upload.wikimedia.org/wikipedia/commons/thumb/d/df/NYC_Empire_State_Building.jpg/640px-NYC_Empire_State_Building.jpg",
-    address: "20 W 34th St, New York, NY 10001",
-    location: { lat: 40.7484405, lng: -73.9878584 },
-    creator: "u1",
-  },
-];
+router.get("/:pid", placesControllers.getPlaceById);
 
-router.get("/:pid", (req, res, next) => {
-  console.log("Get Request in Places");
-  const placeId = req.params.pid;
-  const place = DUMMY_PLACES.find((p) => p.id === placeId);
-
-  if (!place) {
-    throw new HttpError("Could not find a place for the provided id.", 404);
-  }
-
-  res.json({ place });
-});
-
-router.get("/user/:uid", (req, res, next) => {
-  const userId = req.params.uid;
-  const place = DUMMY_PLACES.find((p) => p.creator === userId);
-
-  if (!place) {
-    return next(
-      new HttpError("Could not find a place for the provided id.", 404)
-    );
-  }
-
-  res.json({ place });
-});
+router.get("/user/:uid", placesControllers.getPlaceByUserId);
 
 module.exports = router;
