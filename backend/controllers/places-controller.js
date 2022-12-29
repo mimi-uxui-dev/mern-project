381474059451165ab5ab56280661ee2d98f76562
mimi-uxui-dev <uxui.mimi@gmail.com>
2022-12-29 21:33:26 +0100

    validation API input

--- backend/controllers/places-controller.js
+++ backend/controllers/places-controller.js
@@ -1,5 +1,6 @@
 const HttpError = require("../models/http-error");
 const uuid = require("uuid/v4");
+const { validationResult } = require("express-validator");
 
 let DUMMY_PLACES = [
   {
@@ -60,7 +61,14 @@ const getPlacesByUserId = (req, res, next) => {
 };
 
 const createPlace = (req, res, next) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    console.log(errors);
+    throw new HttpError("Invalid inputs passed, please check your data", 422);
+  }
+
   const { title, description, coordinates, address, creator } = req.body;
+
   const createdPlace = {
     id: uuid(),
     title,
@@ -69,7 +77,9 @@ const createPlace = (req, res, next) => {
     address,
     creator,
   };
+
   DUMMY_PLACES.push(createdPlace);
+
   res.status(201).json({ place: createdPlace });
 };
 
