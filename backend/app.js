8017ae405ddeb00d33f0ccb0f93d1e4a24a12a30
mimi-uxui-dev <uxui.mimi@gmail.com>
2022-12-29 16:03:07 +0100

    creating users routes + signup + login

--- backend/app.js
+++ backend/app.js
@@ -2,27 +2,29 @@ const express = require("express");
 const bodyParser = require("body-parser");
 
 const placesRoutes = require("./routes/places-routes");
+const usersRoutes = require("./routes/users-routes");
 const HttpError = require("./models/http-error");
 
 const app = express();
 
 app.use(bodyParser.json());
 
-app.use("/api/places", placesRoutes);
+app.use("/api/places", placesRoutes); // => /api/places...
+app.use("/api/users", usersRoutes);
 
-// handling unsupported routes
 app.use((req, res, next) => {
-  throw new HttpError("Could not find this route.", 404);
+  const error = new HttpError("Could not find this route.", 404);
+  throw error;
 });
 
-// err handler
 app.use((error, req, res, next) => {
   if (res.headerSent) {
     return next(error);
   }
-
   res.status(error.code || 500);
-  res.json({ message: error.message || "An unknown error occurred!" });
+  res.json({
+    message: error.message + " Ya mimi" || "An unknown error occurred!",
+  });
 });
 
 app.listen(5000);
