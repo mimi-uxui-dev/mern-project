1058a58e9b1baf863d9ce5e6b27a08b1c839cd39
mimi-uxui-dev <uxui.mimi@gmail.com>
2022-12-29 11:46:34 +0100

    handelling errors for unsupported routes

--- backend/app.js
+++ backend/app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 
 const placesRoutes = require("./routes/places-routes");
+const HttpError = require("./models/http-error");
 
 const app = express();
 
@@ -9,6 +10,12 @@ app.use(bodyParser.json());
 
 app.use("/api/places", placesRoutes);
 
+// handling unsupported routes
+app.use((req, res, next) => {
+  throw new HttpError("Could not find this route.", 404);
+});
+
+// err handler
 app.use((error, req, res, next) => {
   if (res.headerSent) {
     return next(error);
