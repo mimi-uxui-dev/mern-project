4d9b5c4d1041de2ff71a5c46bb4b97c05432be7a
mimi-uxui-dev <uxui.mimi@gmail.com>
2022-12-29 10:38:59 +0100

    adding error model

--- backend/routes/places-routes.js
+++ backend/routes/places-routes.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const HttpError = require("../models/http-error");
 
 const router = express.Router();
 
@@ -21,9 +22,7 @@ router.get("/:pid", (req, res, next) => {
   const place = DUMMY_PLACES.find((p) => p.id === placeId);
 
   if (!place) {
-    const error = new Error("Could not find a place for the provided id.");
-    error.code = 404;
-    throw error;
+    throw new HttpError("Could not find a place for the provided id.", 404);
   }
 
   res.json({ place });
@@ -34,9 +33,9 @@ router.get("/user/:uid", (req, res, next) => {
   const place = DUMMY_PLACES.find((p) => p.creator === userId);
 
   if (!place) {
-    const error = new Error("Could not find a place for the provided id.");
-    error.code = 404;
-    next(error);
+    return next(
+      new HttpError("Could not find a place for the provided id.", 404)
+    );
   }
 
   res.json({ place });
