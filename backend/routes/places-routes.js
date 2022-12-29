746dd0e362507fac92e0701b924035cb3954c81c
mimi-uxui-dev <uxui.mimi@gmail.com>
2022-12-29 07:14:03 +0100

    adding a place specific routes

--- backend/routes/places-routes.js
+++ backend/routes/places-routes.js
@@ -2,9 +2,27 @@ const express = require('express')
 
 const router = express.Router()
 
-router.get('/', (req, res, next) => {
+const DUMMY_PLACES = [
+    {
+      id: 'p1',
+      title: 'Empire State Building',
+      description: 'One of the most famous sky scrapers in the world!',
+      imageUrl:
+        'https://upload.wikimedia.org/wikipedia/commons/thumb/d/df/NYC_Empire_State_Building.jpg/640px-NYC_Empire_State_Building.jpg',
+      address: '20 W 34th St, New York, NY 10001',
+      location: {
+        lat: 40.7484405,
+        lng: -73.9878584
+      },
+      creator: 'u1'
+    }
+];
+
+router.get('/:pid', (req, res, next) => {
     console.log('Get Request in Places')
-    res.json({ message: 'it workds'})
+    const placeId = req.params.pid
+    const place = DUMMY_PLACES.find(p => p.id === placeId)
+    res.json({ place })
 })
 
 module.exports = router
